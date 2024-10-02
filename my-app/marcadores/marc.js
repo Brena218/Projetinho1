efda652201769391972e94568fe390bff1253cad
Brena <fbrena218@gmail.com>
2024-10-02 15:46:09 -0300

    feito

---
+++ my-app/marcadores/marc.js
@@ -0,0 +1,32 @@
+export const location = {
+    //Alagoas
+    penedo : 'Penedo',
+    riolargo : 'Rio Largo',
+    arapiraca : 'Arapiraca',
+    saoMiguel : 'Barra de São Miguel',
+    flexeiras : 'Flexeiras',
+
+    //Pará
+   castanhal : 'Castanhal',
+   santaIzabel : 'Santa Izabel do Pará',
+   acara : 'Acará',
+   rendencao : 'Redenção',
+   soure : 'Soure'
+}
+
+export const coord = {
+      //Alagoas
+    longPenedo: "-36.5861", latPenedo: " -10.2906",
+    longRioLargo: "-35.7639", latRioLargo: " -9.6783",
+    longArapiraca: "-36.6616", latArapiraca: " -9.7502",
+    longBarraDeSaoMiguel: " -35.5492", latBarraDeSaoMiguel: " -9.5417",
+    longFlexeiras: "-35.4550", lalFlexeiras: " -9.1632",
+
+    //Pará
+    longCastanhal: "-47.9335", latCastanhal: " -1.2960",
+    longSantaIzabel: "-48.2724", latSantaIzabel: "-1.4123",
+    longAcara: "-47.8454", latAcara: " -1.6122",
+    longRendencao: "-49.4643", latRendencao: "  -8.1857",
+    longSoure: "-48.4740", latSoure: " -0.7780",
+
+}
